0f52544e0a72e8664e46090599670533ed410d94
krystian <krystian.czekalski@resourcekraft.com>
2018-10-13 10:06:10 +0100

    Skonczona autentykacja

--- movierater/urls.py
+++ movierater/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework.authtoken.views import obtain_auth_token
 from django.conf.urls import include
 from api import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(urls))
+    path('api/', include(urls)),
+    path('api-token-auth/', obtain_auth_token)
+]
+
+
+urlpatterns += [
+
 ]
